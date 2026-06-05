2c1ea76b2241f8fed88585bbafd444164e9f89ae
zxx <2434136592@qq.com>
2026-06-05 08:56:46 +0800

    新增了三地址码的相关代码，并且删除了不必一定存在的文件

--- docs/exp2_report.md
+++
@@ -1,3 +0,0 @@
-# Experiment 2 LR1 Report placeholder
-
-See generate script output.
--- scripts/test_word.py
+++
@@ -1,8 +0,0 @@
-import win32com.client
-for prog in ["Word.Application", "Kwps.Application", "wps.Application", "WPS.Application"]:
-    try:
-        w = win32com.client.Dispatch(prog)
-        print("OK", prog, getattr(w, "Name", "?"))
-        w.Quit()
-    except Exception as e:
-        print("FAIL", prog, e)
